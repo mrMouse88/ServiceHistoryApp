6c0cc90213904dda45ba9f2c77f2a5f2f12e34ae
mrMouse <michal.olczyk@op.pl>
2022-03-23 23:58:14 +0100

    AppBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "./App.css";
+
 //components
 import Entries from "./components/Entries";
 
+//materialUI
+import { AppBar, Toolbar, Container, Typography, Button } from "@mui/material";
+
 function App() {
-	return <Entries />;
+	return (
+		<>
+			<AppBar>
+				<Toolbar>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						Ksiąka serwisowa
+					</Typography>
+					<Button color="inherit">Dodaj wpis</Button>
+				</Toolbar>
+			</AppBar>
+			<Container maxWidth="md">
+				<Entries />
+			</Container>
+		</>
+	);
 }
 
 export default App;
